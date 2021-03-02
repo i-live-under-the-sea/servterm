281fec73f25a3545c143637f0360a84e3fb832a6
SpongeBob Squarepants <69375705+i-live-under-the-sea@users.noreply.github.com>
2021-03-01 19:47:30 -0500

    Add files via upload

--- public/main.js
+++ public/main.js
@@ -1,3 +1,24 @@
+function output(){
+	console.log(this.responseText)
+	//var get = nl2br(this.responseText)
+	//document.getElementById("writer").innerHTML += get;
+	txt = this.responseText;
+	if(txt != ""){
+		document.getElementById("setter").value += txt;
+		writeit(document.getElementById("setter"))
+	}else{
+		document.getElementById("setter").value += "Command Not Found \n";
+		writeit(document.getElementById("setter"))
+		writeit(document.getElementById("setter"))
+	}
+}
+
+function doout(txt){
+	//return txt.replace(/\n/g, '<br /><i class="fa fa-terminal" aria-hidden="true"></i><span style="color: white; font-family: ubuntu;"></span>');
+	return txt.replace(/\n/g, '<span style="color: white; font-family: ubuntu;"></span><br />');
+}
+
+
 var cursor = $("#cursor");
 		var setter = $("#setter");
 		var writer = $("#writer");
@@ -12,6 +33,12 @@ var cursor = $("#cursor");
 			var w = $(writer);
 			var tw = from.value;
 			w.html(nl2br(tw));
+		}
+		function writeiit(from, e){
+			e = e || window.event;
+			var w = $(writer);
+			var tw = from.value;
+			w.html(nl2brr(tw));
 		}
 		function moveIt(count, e){
 			e = e || window.event;
@@ -43,8 +70,30 @@ var cursor = $("#cursor");
 				//new line
 				var arr = get.split("\\n");
 				var said = arr[arr.length-2];
-				if(said != ""){
-					
+				if(said != "" && said.includes("cd") == false && said.includes("ls") == false){
+					var oReq = new XMLHttpRequest();
+					oReq.addEventListener("load", output);
+					oReq.open("GET", "ajax.php?shell=" + said);
+					oReq.send();
+				}else if(said.includes("cd")){
+					if(said == "cd /" || said == "cd C:" || said == "cd c:"){
+						localStorage.setItem("path", "/")
+					}else{
+						var input = said.substring(3)
+						if(localStorage.getItem("path") == null){
+							localStorage.setItem("path", input)
+						}else{
+							var inp = localStorage.getItem("path")
+							var putout = inp + "/" + input;
+							localStorage.setItem("path", putout);
+						}
+					}
+					console.log(localStorage.getItem("path"))
+				}else if(said.includes("ls")){
+					var treq = new XMLHttpRequest();
+					treq.addEventListener("load", output);
+					treq.open("GET", "ajax.php?path=" + localStorage.getItem("path"));
+					treq.send();
 				}
 			}
 			moveIt(this.value.length, event);
